1df2f69f24ffae5982735e4557f4c10345021615
Здоренко А.Р. <zdorenko1989@mail.ru>
2024-01-11 10:18:15 +0300

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {TestPage} from "./pages";
 import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <TestPage />,
+    },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/test-page/test-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TestPage = (): React.ReactElement => {
+    return (
+        <div>
+            Hi
+        </div>
+    );
+};
