4d374b22e8be2bb2f9fbb70c8e8d555b0c6f955f
Здоренко А.Р. <zdorenko1989@mail.ru>
2024-01-11 10:22:16 +0300

    add emotion

---
+++ src/pages/test-page/test-page.style.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled'
+
+export const TestPageStyled = styled.div`
+    background-color: #61dafb;
+`
--- src/pages/test-page/test-page.tsx
+++ src/pages/test-page/test-page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { TestPageStyled } from './test-page.style';
 
 export const TestPage = (): React.ReactElement => {
     return (
-        <div>
+        <TestPageStyled>
             Hi
-        </div>
+        </TestPageStyled>
     );
 };
